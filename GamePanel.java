5a1272514e56524a091c641034134422f7bdf48e
Savannah-Lu <savannahlut@nycstudents.net>
2025-01-20 06:35:15 +0000

    Testing: rectangle

--- GamePanel.java
+++ GamePanel.java
@@ -1,8 +1,10 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
 
     //SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
@@ -14,9 +16,37 @@ public class GamePanel extends JPanel{
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    Thread gameThread;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.black);
+        this.setBackground(Color.blue);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override 
+    public void run(){
+        while(gameThread != null){
+        
+            update();
+
+            repaint();
+        }
+    }
+
+    public void update(){
+    
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }
