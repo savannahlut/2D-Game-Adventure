796d6c3bc142d576fea1a94b84b01dac5d1e1b60
Savannah-Lu <savannahlut@nycstudents.net>
2025-01-24 15:01:29 -0500

    Mr. Holmer

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args){
+
+        System.out.println("Hi Savannah");
  
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
